4313f329e8ac00374c840d496eba30709f1b3f71
wocucumber <wocucumber@gmail.com>
2026-09-13 21:36:17 +0900

    Fix a bug that can't access api

--- src/authorication.ts
+++ src/authorication.ts
@@ -5,7 +5,8 @@ export class Authorication {
   constructor({private_key, client_email}: {private_key: string, client_email: string}) {
     this._jwt = new JWT({
       email: client_email,
-      key: private_key
+      key: private_key,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"]
     });
   }
 
