de03d27f94379cbab4b48f0d1ee877810499d75f
wocucumber <wocucumber@gmail.com>
2026-09-16 23:10:55 +0900

    Fix appendRow

--- src/table.ts
+++ src/table.ts
@@ -87,7 +87,7 @@ export class Table<S extends Schema> {
     await row.save();
   }
 
-  async appendRow(value: S) {
+  async appendRow(value: Omit<S, "id">) {
     const rows = await this.sheet.getRows();
     const newId = rows.reduce((prev, curr) => Math.max(prev, Number(curr.get("id"))), 0) + 1;
 
